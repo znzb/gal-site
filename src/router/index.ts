d002dd79cf0762fc739ee7f48549b60a4686b739
znzb <2500575060@qq.com>
2026-05-16 12:32:04 +0800

    使用Hash模式路由修复页面空白问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import GameDetail from '@/views/GameDetail.vue'
 import Category from '@/views/Category.vue'
@@ -147,7 +147,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     return { top: 0 }
